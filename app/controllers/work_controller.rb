0186335424eb562630f2e7acda0f0cb870e74af3
Lee Ho Yean <ghdusjang@naver.com>
2015-08-22 02:38:50 +0000

    html a little

--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -1,4 +1,13 @@
 class WorkController < ApplicationController
     def index
+        @name = [{'name'=>'호연','on' => true},
+                {'name'=>'현우','on' => false},
+                {'name'=>'상우','on' => false}]
+    end
+    
+    def schedule
+        @name = [{'name'=>'호연','on' => true},
+                {'name'=>'현우','on' => false},
+                {'name'=>'상우','on' => false}]
     end
 end
